5fa6f6b3b0ddae10c1b2d894b9fce899896f6137
Minghui Du <duminghhui@hotmail.com>
2026-01-21 19:05:41 +0100

    terraform variable

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,20 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "7.16.0"
     }
   }
 }
 
 provider "google" {
-  project     = "terraform-test-485011"
-  region      = "us-central1"
+  project = var.project
+  region  = var.region
 }
 
 resource "google_storage_bucket" "demo-bucket" {
-  name          = "terraform-test-485011-terra-bucket"
-  location      = "US"
+  name          = var.gcs_bucket_name
+  location      = var.location
   force_destroy = true
 
   lifecycle_rule {
@@ -25,4 +25,9 @@ resource "google_storage_bucket" "demo-bucket" {
       type = "AbortIncompleteMultipartUpload"
     }
   }
+}
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.dq_dataset_name
+  location   = var.location
 }
